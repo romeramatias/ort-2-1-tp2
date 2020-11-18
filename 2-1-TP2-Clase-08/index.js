2f9b92307eafb011b900d8206c4b7b620ed6c0f6
Romera Matias <romeramatias0@gmail.com>
2020-11-18 13:40:48 -0300

    Edit Clase-08

--- 2-1-TP2-Clase-08/index.js
+++ 2-1-TP2-Clase-08/index.js
@@ -1,1 +1,28 @@
-console.log('Hola');
+console.log('Variables de entorno');
+console.log('Comandos Heroku');
+/* 
+heroku config
+heroku open
+git push heroku master
+heroku logs --tail
+heroku create
+heroku login
+heroku --version
+
+heroku login
+heroku create
+git remote -v
+
+heroku config
+git push heroku master
+heroku open
+
+heroku logs --tail
+express --view=hbs apifoxes
+
+
+Al profe
+- Como validar los tipos de los campos
+- Estructura de un proyecto nodejs
+
+*/
